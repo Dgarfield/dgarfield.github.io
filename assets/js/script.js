2ce7f830021f9d71d4d282d017b5c83d3c6cc9f2
Damon Garfield <damongarfield@gmail.com>
2023-09-02 23:47:03 -0600

    Code cleanup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,60 @@
 {
   'use strict';
+  const bodyEl = document.body;
+  const lightSwitchEl = document.getElementById("light-switch");
+  const mainHeaderEl = document.getElementById("main-header");
+  const footerEl = document.getElementById("footer");
+  const neonBorderEl = document.querySelectorAll('.neon-border');
+  const spacersEl = document.querySelectorAll('.spacer');
 
-  const toggleClass = (el, cssClass) => {
-    el.classList.toggle(cssClass);
+  const toggleTheme = (theme='none') => {
+    const lightOn = lightSwitchEl.classList.contains("light-on");
+    
+    if ((theme === 'light' && !lightOn) || (theme === 'dark' && lightOn) || (theme === 'none')) {
+      bodyEl.classList.toggle("light-on");
+      lightSwitchEl.classList.toggle("light-on");
+      mainHeaderEl.classList.toggle("light-on");
+      footerEl.classList.toggle("light-on");
+      neonBorderEl.forEach((element) => {
+        element.classList.toggle("light-on");
+      });
+    }
   };
+
   const styleElements = () => {
     /* Fixed position headers mess up anchors, so this fix is to make anchors jump to correct positions */
     const headerHeight = document.getElementById('main-header').offsetHeight;
-    const footerHeight = document.getElementById('footer').offsetHeight;
-    const spacers = document.querySelectorAll('.spacer');
     
-    spacers.forEach((element) => {
-      element.style.height = `${footerHeight / 10}rem`;
+    spacersEl.forEach((element) => {
+      element.style.height = `${headerHeight / 12}rem`;
     });
-    document.getElementById("home-a").style.height = `${headerHeight / 11}rem`;
+    document.getElementById("home-a").style.height = `${headerHeight / 10}rem`;
+  };
 
+  let typeWriterInt = 0;
+  let timeoutID = {};
+  const typeWriterTxt = "I build responsive websites.\nPlease check out my skills below.";
+  const typeWriterTxtLen = typeWriterTxt.length;
+
+  const typeWriter = () => {
+    if (typeWriterInt < typeWriterTxtLen) {
+      document.getElementById("typing").innerHTML += typeWriterTxt.charAt(typeWriterInt);
+      typeWriterInt++;
+      timeoutID = setTimeout (
+        typeWriter, 60
+      );
+    } else {
+      clearTimeout(timeoutID);
+    }
+  };
+
+  const themeToggleBtn = document.querySelector("[data-theme-btn]");
+  themeToggleBtn.addEventListener("click", () => {
+    toggleTheme();
+  });
+
+  window.onload = () => {
+    styleElements();
     typeWriter();
 
     const d = new Date();
@@ -22,67 +62,12 @@
     document.getElementById("year").innerHTML = year;
   };
 
-  let i = 0;
-  const typeWriter = () => {
-    const txt = "I build responsive websites.\nPlease check out my skills below.";
-    const speed = 80;
-    let timeoutID = null;
-
-    if (i < txt.length) {
-      document.getElementById("typing").innerHTML += txt.charAt(i);
-      i++;
-      timeoutID = setTimeout (
-        typeWriter, speed
-      );
-    } else {
-      clearTimeout(timeoutID);
-    }
-  };
-
-  window.onload = () => {
-    styleElements();
-  };
   window.onresize = () => {
     styleElements();
   };
-
-  /* Set color scheme by user preference, if they've set it up in their browser */
+  
   window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
     const theme = e.matches ? "dark" : "light";
-    const lightSwitch = document.getElementById("light-switch");
-    const on = lightSwitch.classList.contains("light-on");
-    const el = document.body;
-    const el0 = document.getElementById("main-header");
-    const el1 = document.getElementById("footer");
-    const elements = document.querySelectorAll('.neon-border');
-    
-    if ((theme === 'light' && !on) || (theme === 'dark' && on)) {
-      toggleClass(el, "light-on");
-      toggleClass(lightSwitch, "light-on");
-      toggleClass(el0, "light-on");
-      toggleClass(el1, "light-on");
-
-      elements.forEach((element) => {
-        toggleClass(element, "light-on");
-      });
-    }
-  });
-
-  const themeToggleBtn = document.querySelector("[data-theme-btn]");
-  themeToggleBtn.addEventListener("click", () => {
-    const el = document.body;
-    const el0 = document.getElementById("light-switch");
-    const el1 = document.getElementById("main-header");
-    const el2 = document.getElementById("footer");
-    const elements = document.querySelectorAll('.neon-border');
-
-    toggleClass(el, "light-on");
-    toggleClass(el0, "light-on");
-    toggleClass(el1, "light-on");
-    toggleClass(el2, "light-on");
-    
-    elements.forEach((element) => {
-      toggleClass(element, "light-on");
-    });
+    toggleTheme(theme);
   });
 }
