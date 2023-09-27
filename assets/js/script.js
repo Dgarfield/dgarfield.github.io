d5c9c8ad2b6bc0cec396fc26d76161414d082c04
Damon Garfield <damongarfield@gmail.com>
2023-09-26 18:12:03 -0600

    Updated the home icon, removed clouds & optimized

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,50 @@
 {
   'use strict';
   
-  const bodyEl = document.body;
-  const lightSwitchEl = document.getElementById("light-switch");
-  const mainHeaderEl = document.getElementById("main-header");
-  const footerEl = document.getElementById("footer");
-  const neonBorderEl = document.querySelectorAll('.neon-border');
-  const spacersEl = document.querySelectorAll('.spacer');
+  const els = {
+    body: document.body,
+    lightSwitch: document.getElementById("light-switch"),
+    mainHeader: document.getElementById("main-header"),
+    homeSpacer: document.getElementById("home-a"),
+    typing: document.getElementById("typing"),
+    footer: document.getElementById("footer"),
+    curYear: document.getElementById("year"),
+    themeBtn: document.querySelector("[data-theme-btn]"),
+    neonBorder: document.querySelectorAll('.neon-border'),
+    spacers: document.querySelectorAll('.spacer'),
 
-  const toggleTheme = (theme='none') => {
-    const lightOn = lightSwitchEl.classList.contains("light-on");
-    
-    if ((theme === 'light' && !lightOn) || (theme === 'dark' && lightOn) || (theme === 'none')) {
-      bodyEl.classList.toggle("light-on");
-      lightSwitchEl.classList.toggle("light-on");
-      mainHeaderEl.classList.toggle("light-on");
-      footerEl.classList.toggle("light-on");
-      neonBorderEl.forEach((element) => {
-        element.classList.toggle("light-on");
+    toggleTheme: function(theme='none') {
+      const lightOn = this.lightSwitch.classList.contains("light-on");
+      
+      if ((theme === 'light' && !lightOn) || (theme === 'dark' && lightOn) || (theme === 'none')) {
+        this.body.classList.toggle("light-on");
+        this.lightSwitch.classList.toggle("light-on");
+        this.mainHeader.classList.toggle("light-on");
+        this.footer.classList.toggle("light-on");
+        this.neonBorder.forEach((element) => {
+          element.classList.toggle("light-on");
+        });
+      }
+    },
+    styleElements: function() {
+      /* Fixed position headers mess up anchors, so this fix is to make anchors jump to correct positions */
+      const headerHeight = this.mainHeader.offsetHeight;
+      
+      els.spacers.forEach((element) => {
+        element.style.height = `${headerHeight / 12}rem`;
       });
+      this.homeSpacer.style.height = `${headerHeight / 10}rem`;
     }
-  };
-
-  const styleElements = () => {
-    /* Fixed position headers mess up anchors, so this fix is to make anchors jump to correct positions */
-    const headerHeight = document.getElementById('main-header').offsetHeight;
-    
-    spacersEl.forEach((element) => {
-      element.style.height = `${headerHeight / 12}rem`;
-    });
-    document.getElementById("home-a").style.height = `${headerHeight / 10}rem`;
-  };
+  }
 
   let typeWriterInt = 0;
   let timeoutID = {};
-  const typeWriterTxt = "I build responsive websites.\nPlease check out my skills below.";
-  const typeWriterTxtLen = typeWriterTxt.length;
-
   const typeWriter = () => {
+    const typeWriterTxt = "I build responsive websites.\nPlease check out my skills below.";
+    const typeWriterTxtLen = typeWriterTxt.length;
+
     if (typeWriterInt < typeWriterTxtLen) {
-      document.getElementById("typing").innerHTML += typeWriterTxt.charAt(typeWriterInt);
+      els.typing.innerHTML += typeWriterTxt.charAt(typeWriterInt);
       typeWriterInt++;
       timeoutID = setTimeout (
         typeWriter, 60
@@ -49,26 +54,26 @@
     }
   };
 
-  const themeToggleBtn = document.querySelector("[data-theme-btn]");
+  const themeToggleBtn = els.themeBtn;
   themeToggleBtn.addEventListener("click", () => {
-    toggleTheme();
+    els.toggleTheme();
   });
 
   window.onload = () => {
-    styleElements();
+    els.styleElements();
     typeWriter();
 
     const d = new Date();
     let year = d.getFullYear();
-    document.getElementById("year").innerHTML = year;
+    els.curYear.innerHTML = year;
   };
 
   window.onresize = () => {
-    styleElements();
+    els.styleElements();
   };
   
   window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
     const theme = e.matches ? "dark" : "light";
-    toggleTheme(theme);
+    els.toggleTheme(theme);
   });
 }
