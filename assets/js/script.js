3e980866083a2707ac49f6d0fcf9fa30a73477db
dgarfield <damongarfield@gmail.com>
2025-08-10 02:51:07 -0600

    Updates to match my resume as well as styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,17 @@ const els = {
   footer: document.getElementById("footer"),
   curYear: document.getElementById("year"),
   themeBtn: document.querySelector("[data-theme-btn]"),
-  neonBorder: document.querySelectorAll('.neon-border'),
-  spacers: document.querySelectorAll('.spacer'),
+  neonBorder: document.querySelectorAll(".neon-border"),
+  spacers: document.querySelectorAll(".spacer"),
 
-  toggleTheme: function(theme='none') {
+  toggleTheme: function (theme = "none") {
     const lightOn = this.lightSwitch.classList.contains("light-on");
-    
-    if ((theme === 'light' && !lightOn) || (theme === 'dark' && lightOn) || (theme === 'none')) {
+
+    if (
+      (theme === "light" && !lightOn) ||
+      (theme === "dark" && lightOn) ||
+      theme === "none"
+    ) {
       this.body.classList.toggle("light-on");
       this.lightSwitch.classList.toggle("light-on");
       this.mainHeader.classList.toggle("light-on");
@@ -23,29 +27,28 @@ const els = {
       });
     }
   },
-  styleElements: function() {
+  styleElements: function () {
     /* Fixed position headers mess up anchors, so this fix is to make anchors jump to correct positions */
     const headerHeight = this.mainHeader.offsetHeight;
-    
+
     els.spacers.forEach((element) => {
       element.style.height = `${headerHeight / 12}rem`;
     });
     this.homeSpacer.style.height = `${headerHeight / 10}rem`;
-  }
-}
+  },
+};
 
 let typeWriterInt = 0;
 let timeoutID = {};
 const typeWriter = () => {
-  const typeWriterTxt = "I've been a Front End Web Developer for over 10 years.\nPlease check out some of my skills below.";
+  const typeWriterTxt =
+    "I've been a Web Developer for over 15 years!\nI kicked off my career at a paintball company before thriving\xa0at\xa0a\xa0large\xa0agency.\nThen during Covid moved on to working with cutting edge technology,\xa0at\xa0an\xa0AI\xa0company.\n\nPlease check out some of my skills below.\n\n";
   const typeWriterTxtLen = typeWriterTxt.length;
 
   if (typeWriterInt < typeWriterTxtLen) {
     els.typing.innerHTML += typeWriterTxt.charAt(typeWriterInt);
     typeWriterInt++;
-    timeoutID = setTimeout (
-      typeWriter, 60
-    );
+    timeoutID = setTimeout(typeWriter, 10);
   } else {
     clearTimeout(timeoutID);
   }
@@ -69,7 +72,9 @@ window.onresize = () => {
   els.styleElements();
 };
 
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-  const theme = e.matches ? "dark" : "light";
-  els.toggleTheme(theme);
-});
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (e) => {
+    const theme = e.matches ? "dark" : "light";
+    els.toggleTheme(theme);
+  });
